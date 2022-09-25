1ea4472bdff83bd6d71d7c2134018c27ace7b55b
BSerge <serge.boskin@icloud.com>
2022-09-25 12:48:45 +0300

    add Objective_23

---
+++ Objective_23/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите целое число: ");
+int number = int.Parse(Console.ReadLine());
+
+for (int index = 1; index <= number; index++)
+{
+    double result = Math.Pow(index,3);
+    Console.Write($"{result} ");
+}
