dbfbe2531e8ce70531e9bd8f7daf6d6b63b7e1aa
BSerge <serge.boskin@icloud.com>
2022-09-25 12:19:58 +0300

    add Objective_19

---
+++ Objective_19/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите натуральное число: ");
+int number = int.Parse(Console.ReadLine());
+int p = 0; 
+int num = number;
+
+while (number > 0) 
+{
+    p = p * 10 + number % 10; 
+    number = number / 10;
+}
+
+Console.WriteLine(num == p ? "Палиндром" : "Не палиндром");
